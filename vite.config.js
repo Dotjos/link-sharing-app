c55e754012d1cef8c9b286b2c02d827231257078
Dotjos <oladotjos@gmail.com>
2024-02-22 15:39:37 +0000

    hmmmm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define:{
-    VITE_APP_URL : import.meta.env.VITE_APP_URL,
-    VITE_SUPABASE_KEY : import.meta.env.VITE_SUPABASE_KEY
+    VITE_APP_URL : process.env.VITE_APP_URL,
+    VITE_SUPABASE_KEY : process.env.VITE_SUPABASE_KEY
   }
 })
