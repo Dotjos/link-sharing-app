beaf3018afec8b625ef009760b0dbaefff6a66a4
Jose <oladotjos@gmail.com>
2025-09-27 18:15:59 +0100

    update-Input-password-accessibility

--- frontend/src/ui/SignInput.jsx
+++ frontend/src/ui/SignInput.jsx
@@ -3,36 +3,14 @@ import { MdOutlineRemoveRedEye  } from "react-icons/md";
 import { IoEyeOffOutline } from "react-icons/io5";
 <IoEyeOffOutline />
 
-// function SignInput ({icon,placeholder,type,label,name,error,onChange,value,errMessage,disabled}){
-
-//   function eyeClick(){
-//     type="text"
-//     console.log(type);
-//   }
-
-//   return ( 
-//     <div className="my-4 focus:shadow-xl">
-//      <label htmlFor={name}>
-//       <div className="flex justify-between">
-//       <span className="">{label}</span>
-//       {error&&<span className="text-right lg:w-1/4  text-LightRed text-xs">{errMessage}</span>}
-//         </div>   
-//             <div className= {`border ${error?"border-LightRed":""} rounded-lg hover:border-NeonBlue hover:shadow-MaximumBluePurple hover:shadow-md flex gap-3 p-2`}>
-//             <img src={icon}/>
-//             <input id={name} name={name}  onChange={onChange} disabled={disabled} type={type} value={value} className="w-full outline-none bg-inherit" placeholder={placeholder}/>
-//             {eyeAble&&<MdOutlineRemoveRedEye className="w-7 h-7 p-0.5" onClick={eyeClick}/>}
-//             </div>
-//        </label>
-//     </div>
-//   );
-// }
-
-function SignInput({ icon, placeholder, type, label, name, error, onChange, value, errMessage, disabled, eyeAble }) {
+function SignInput({ icon, placeholder, type, label, name, error, onChange, value, errMessage, disabled, eyeAble,autoComplete="off" }) {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
+  
   const togglePasswordVisibility = () => {
     setIsPasswordVisible((prev) => !prev);
   };
+
+  const inputType = eyeAble && isPasswordVisible ? "text" : type;
 
   return (
     <div className="my-4 focus:shadow-xl">
@@ -46,9 +24,11 @@ function SignInput({ icon, placeholder, type, label, name, error, onChange, valu
           <input
             id={name}
             name={name}
+            aria-invalid={!!error}
+            autoComplete={autoComplete}
             onChange={onChange}
             disabled={disabled}
-            type={isPasswordVisible ? 'text' : type} // Update type based on isPasswordVisible
+            type={inputType} // Update type based on isPasswordVisible
             value={value}
             className="w-full outline-none bg-inherit"
             placeholder={placeholder}
@@ -63,9 +43,6 @@ function SignInput({ icon, placeholder, type, label, name, error, onChange, valu
     </div>
   );
 }
-
-
-
 
 
 export default SignInput;
