e3a46e2ef2cb6b4ae2dcd8f8bdb96084ce011b73
Jose <oladotjos@gmail.com>
2025-10-07 22:08:13 +0100

    updated-include-placeholder

--- frontend/src/utilitis/LinkInfo.js
+++ frontend/src/utilitis/LinkInfo.js
@@ -1,71 +1,93 @@
-export const platformDetails=[
-    {
-    platform:"Github",
+export const platformDetails = [
+  {
+    platform: "Github",
     img: "/icon-github.svg",
-    color:"bg-github",
+    color: "bg-github",
     platFormat: /^https?:\/\/(www\.)?github\.com/,
-    },{
-      platform:"Youtube",
-      img: "/icon-youtube.svg",
-      color:"bg-red-400",
-      platFormat: /^https?:\/\/(www\.)?youtube\.com/,
-    },{
-      platform:"Frontend Mentor",
-      img: "/icon-frontend-mentor.svg",
-      color:"bg-frontendmentors",
-      platFormat: /^https?:\/\/(www\.)?frontendmentor\.io/,
-    },{
-      platform:"X",
-      img: "/icon-twitter.svg",
-      color:"bg-twitter",
-      platFormat: /^https?:\/\/(www\.)?twitter\.com/,
-    },
-      {
-      platform:"LinkedIn",
-      img: "/icon-linkedin.svg",
-      color:"bg-LinkedIn",
-      platFormat: /^https?:\/\/(www\.)?linkedin\.com/,
-    },{
-      platform:"Facebook",
-      img: "/icon-facebook.svg",
-      color:"bg-facebook",
-      platFormat: /^https?:\/\/(www\.)?facebook\.com/,
-    },{
-      platform:"Twitch",
-      img:"/icon-twitch.svg",
-      color:"bg-twitch",
-      platFormat: /^https?:\/\/(www\.)?twitch\.tv/,
-
-    },{
-      platform:"Dev.to",
-      img: "/icon-devto.svg",
-      color:"bg-DevTo",
-      platFormat: /^https?:\/\/(www\.)?dev\.to/,
-    },{
-      platform:"Codewars",
-      img: "/icon-codewars.svg",
-      color:"bg-Codeward",
-      platFormat: /^https?:\/\/(www\.)?codewars\.io/,
-
-    },{
-      platform:"freeCodeCamp",
-      img: "/icon-freecodecamp.svg",
-      color:"bg-freeCodeCamp",
-      platFormat: /^https?:\/\/(www\.)?freecodecamp\.org/,      
-        },{
-      platform:"GitLab",
-      img: "/icon-gitlab.svg",
-      color:"bg-gitLab",
-      platFormat: /^https?:\/\/(www\.)?gitlab\.com/,
-    },{
-      platform:"Hashnode",
-      img:"/icon-hashnode.svg",
-      color:"bg-Hashnode",
-      platFormat: /^https?:\/\/(www\.)?hashnode\.com/,
-    },{
-      platform:"Stack Overflow",
-      img: "/icon-stack-overflow.svg",
-      color:"bg-stackOverflow",
-      platFormat: /^https?:\/\/(www\.)?stackoverflow\.com/,
-    }
-  ]
+    placeholder: "e.g. https://github.com/Dotjos",
+  },
+  {
+    platform: "Youtube",
+    img: "/icon-youtube.svg",
+    color: "bg-red-400",
+    platFormat: /^https?:\/\/(www\.)?youtube\.com/,
+    placeholder: "e.g. https://www.youtube.com/@yourchannel",
+  },
+  {
+    platform: "Frontend Mentor",
+    img: "/icon-frontend-mentor.svg",
+    color: "bg-frontendmentors",
+    platFormat: /^https?:\/\/(www\.)?frontendmentor\.io/,
+    placeholder: "e.g. https://www.frontendmentor.io/profile/yourusername",
+  },
+  {
+    platform: "X",
+    img: "/icon-twitter.svg",
+    color: "bg-twitter",
+    platFormat: /^https?:\/\/(www\.)?twitter\.com/,
+    placeholder: "e.g. https://twitter.com/yourhandle",
+  },
+  {
+    platform: "LinkedIn",
+    img: "/icon-linkedin.svg",
+    color: "bg-LinkedIn",
+    platFormat: /^https?:\/\/(www\.)?linkedin\.com/,
+    placeholder: "e.g. https://www.linkedin.com/in/yourprofile",
+  },
+  {
+    platform: "Facebook",
+    img: "/icon-facebook.svg",
+    color: "bg-facebook",
+    platFormat: /^https?:\/\/(www\.)?facebook\.com/,
+    placeholder: "e.g. https://www.facebook.com/yourprofile",
+  },
+  {
+    platform: "Twitch",
+    img: "/icon-twitch.svg",
+    color: "bg-twitch",
+    platFormat: /^https?:\/\/(www\.)?twitch\.tv/,
+    placeholder: "e.g. https://www.twitch.tv/yourchannel",
+  },
+  {
+    platform: "Dev.to",
+    img: "/icon-devto.svg",
+    color: "bg-DevTo",
+    platFormat: /^https?:\/\/(www\.)?dev\.to/,
+    placeholder: "e.g. https://dev.to/yourusername",
+  },
+  {
+    platform: "Codewars",
+    img: "/icon-codewars.svg",
+    color: "bg-Codeward",
+    platFormat: /^https?:\/\/(www\.)?codewars\.com/,
+    placeholder: "e.g. https://www.codewars.com/users/yourusername",
+  },
+  {
+    platform: "freeCodeCamp",
+    img: "/icon-freecodecamp.svg",
+    color: "bg-freeCodeCamp",
+    platFormat: /^https?:\/\/(www\.)?freecodecamp\.org/,
+    placeholder: "e.g. https://www.freecodecamp.org/yourusername",
+  },
+  {
+    platform: "GitLab",
+    img: "/icon-gitlab.svg",
+    color: "bg-gitLab",
+    platFormat: /^https?:\/\/(www\.)?gitlab\.com/,
+    placeholder: "e.g. https://gitlab.com/yourusername",
+  },
+  {
+    platform: "Hashnode",
+    img: "/icon-hashnode.svg",
+    color: "bg-Hashnode",
+    platFormat: /^https?:\/\/(www\.)?hashnode\.com/,
+    placeholder: "e.g. https://hashnode.com/@yourusername",
+  },
+  {
+    platform: "Stack Overflow",
+    img: "/icon-stack-overflow.svg",
+    color: "bg-stackOverflow",
+    platFormat: /^https?:\/\/(www\.)?stackoverflow\.com/,
+    placeholder: "e.g. https://stackoverflow.com/users/yourid/yourname",
+  },
+];
